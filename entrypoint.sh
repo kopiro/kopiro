dc0e0e8a4eaa7df7abcb8039018cc6d7293c3235
flavio.destefano <flavio.destefano@caffeina.com>
2017-12-28 14:36:38 +0100

    Rebuild only if params def

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-rm -rf /app/vendor && ln -svf /composer/vendor
-rm -rf /app/node_modules && ln -svf /node/node_modules /app/node_modules
-npm run build
+if [ "$REBUILD" == "1" ]; then
+	rm -rf /app/vendor && ln -svf /composer/vendor
+	rm -rf /app/node_modules && ln -svf /node/node_modules /app/node_modules
+	npm run build
+fi
 supervisord -c /etc/supervisor/conf.d/supervisord.conf
