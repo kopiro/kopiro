a93367876746e7562d742eb198eb31cdc4040edb
Flavio De Stefano <destefano.flavio@gmail.com>
2018-11-16 10:17:49 +0100

    Adding renderer

--- render/index.js
+++ render/index.js
@@ -1,12 +1,22 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
+async function run() {
 	const browser = await puppeteer.launch({
 		args: ['--no-sandbox', '--disable-setuid-sandbox']
 	});
 	const page = await browser.newPage();
-	await page.goto('http://app', { waitUntil: 'networkidle2' });
-	await page.emulateMedia('screen');
-	await page.pdf({ path: '/screenshots/cv.pdf' });
+	const response = await page.goto('http://nginx', {
+		waitUntil: 'networkidle2'
+	});
+	if (response.status() == 200) {
+		await page.emulateMedia('screen');
+		await page.pdf({
+			margin: { left: '2cm', top: '2.5cm', right: '2cm', bottom: '2.5cm' },
+			path: '/screenshots/cv.pdf'
+		});
+	}
 	await browser.close();
-})();
+}
+
+run();
+setInterval(run, 60 * 1000);
