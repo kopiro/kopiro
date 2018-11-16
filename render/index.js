e1bdec9de539887679b6fc74c66f621eb90b867e
Flavio De Stefano <destefano.flavio@gmail.com>
2018-11-16 11:23:58 +0100

    Edited docekr-compose prod

--- render/index.js
+++ render/index.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 
 async function run() {
+	console.log('Screenshot run');
 	const browser = await puppeteer.launch({
 		args: ['--no-sandbox', '--disable-setuid-sandbox']
 	});
@@ -14,9 +15,14 @@ async function run() {
 			margin: { left: '2cm', top: '2.5cm', right: '2cm', bottom: '2.5cm' },
 			path: '/screenshots/cv.pdf'
 		});
+		console.log('Screenshot OK');
+	} else {
+		console.log('Screenshot gave error');
 	}
 	await browser.close();
 }
+
+console.log('Screenshot started');
 
 run();
 setInterval(run, 60 * 1000);
